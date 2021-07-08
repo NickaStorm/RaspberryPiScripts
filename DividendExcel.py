547121ba7b18e3449d3c3e5465341992236d8671
NickaStorm <nicka@ai-engineered.com>
2021-07-08 11:46:58 -0600

    Working variation of converting xlsx to csv, need to print and transfer info to a SQL table using sqlite3

--- DividendExcel.py
+++ DividendExcel.py
@@ -1,5 +1,17 @@
-import xlrd
+# from openpyxl import load_workbook
+#
+# data_file = r'c:\users\nicka\pycharmprojects\raspberrypiscripts\usdividendchampions.xlsx'
+#
+# # Load the entire workbook.
+# wb = load_workbook(data_file)
+#
+# # List all the sheets in the file.
+# print("Found the following worksheets:")
+# for sheetname in wb.sheetnames:
+#     print(sheetname)
 
-excel = xlrd.open_workbook('USDividendChampions.xls')
 
-print(excel)
+
+import pandas as pd
+data_xls = pd.read_excel(r'c:\users\nicka\pycharmprojects\raspberrypiscripts\usdividendchampions.xlsx', 'Champions', engine='openpyxl')
+data_xls.to_csv('csvfile.csv', encoding='utf-8')
