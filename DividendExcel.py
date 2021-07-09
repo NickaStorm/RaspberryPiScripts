80d9e41196d8d81a4a1208bea79072daba4337cb
NickaStorm <nicka@ai-engineered.com>
2021-07-09 12:23:14 -0600

    Updated database function to work with SQLite3 on the execute commands

--- DividendExcel.py
+++ DividendExcel.py
@@ -17,8 +17,8 @@ def insertdata(info, table1, table2):
     for row in info:
         stockinfo = info[indexnumber].tolist()
         yearsonlist = stockinfo.pop(3)
-        cur.execute(f'insert into {table1} (stockname, stickersymbol, sector, industry) values ({stockinfo[1]}, {stockinfo[0]}, {stockinfo[2]}, {stockinfo[3]})')
-        cur.execute(f'insert into {table2} (dateofinfo, yearsonlist) values (curdate(), {yearsonlist})')
+        cur.execute(f"insert into {table1} (stockname, stickersymbol, sector, industry) values ('{stockinfo[1]}', '{stockinfo[0]}', '{stockinfo[2]}', '{stockinfo[3]}');")
+        cur.execute(f"insert into {table2} (dateofinfo, yearsonlist) values (curdate(), '{yearsonlist}');")
         indexnumber += 1
 
 
