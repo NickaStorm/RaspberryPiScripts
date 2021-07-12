3bdb9b16c5cee95d5cde622eddd4e088f512e691
NickaStorm <nicka@ai-engineered.com>
2021-07-12 16:03:17 -0600

    Corrected the pandas execute command syntax, and removed unnecessary parameters from the insertdata function

--- DividendExcel.py
+++ DividendExcel.py
@@ -13,18 +13,18 @@ data = pd.read_csv('csvfile.csv')
 con = sqlite3.connect('dividendchampions.db')
 cur = con.cursor()
 
-def insertdata(info, table1, table2):
+def insertdata(info):
     indexnumber = 0
     for row in info:
         stockinfo = info[indexnumber].tolist()
         yearsonlist = stockinfo.pop(3)
-        cur.execute(f"insert into {table1} (stockname, stickersymbol, sector, industry) values ('{stockinfo[1]}', '{stockinfo[0]}', '{stockinfo[2]}', '{stockinfo[3]}')")
-        cur.execute(f"insert into {table2} (dateofinfo, yearsonlist) values (curdate(), '{yearsonlist}')")
+        cur.execute("insert into stockinfo (stockname, stickersymbol, sector, industry) values (?, ?, ?, ?)", stockinfo)
+        cur.execute("insert into stockdates (dateofinfo, yearsonlist) values (?, ?)", ('curdate()', yearsonlist))
         indexnumber += 1
 
 
 
-insertdata(data.iloc, 'stockinfo', 'stockdates')
+insertdata(data.iloc)
 
 con.commit()
 con.close()
