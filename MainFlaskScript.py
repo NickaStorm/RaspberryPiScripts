13ea2cf61fa19e2b87d2ec1c857185bff0826b0c
NickaStorm <nicka@ai-engineered.com>
2021-07-23 16:16:49 -0600

    testing creating sub pages

--- MainFlaskScript.py
+++ MainFlaskScript.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#creates the base home page
 @app.route('/')
 def index():
     return 'Hello world'
 
+#creates the table sub page
+@app.route('/tables')
+def tables():
+    return 'insert table here'
+
+#runs the app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
