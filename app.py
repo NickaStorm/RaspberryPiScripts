3da878f38130f7efa9a2f4aa4ba19e7e9f56cb44
NickaStorm <nicka@ai-engineered.com>
2021-07-23 16:36:00 -0600

    renamed the main web app file and the html file to help with troubleshooting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #creates a web app at http://127.0.0.1:5000 then add any sub pages
 app = Flask(__name__)
@@ -6,12 +6,12 @@ app = Flask(__name__)
 #creates the base home page
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Home page'
 
 #creates the table sub page
 @app.route('/tables')
 def tables():
-    return 'insert table here'
+    return render_template('index.html')
 
 #runs the app
 if __name__ == '__main__':
