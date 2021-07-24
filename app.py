04405b445161e6bccd8eb5f2b55a0c63e8fcf075
NickaStorm <nicka@ai-engineered.com>
2021-07-24 14:38:09 -0600

    testing  using flask instead of javascript to read sql tables

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 from flask import Flask, render_template
+import json
+import mariadb
 
 #this script needs to be in home/pi/webapp along with the templates dir
 
 #creates a web app at http://127.0.0.1:5000 then add any sub pages
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
+config = {
+    'host': '127.0.0.1',
+    'port': 5000,
+    'user': 'root',
+    'password': 'blueberry',
+    'database': 'dividendchampions'
+}
 
 #creates the base home page
 @app.route('/')
 def index():
-    return 'Home page'
+    # connection for MariaDB
+    conn = mariadb.connect(**config)
+    # create a connection cursor
+    cur = conn.cursor()
+    # execute a SQL statement
+    cur.execute("select * from stockinfo")
+
+    # serialize results into JSON
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+
+    # return the results!
+    return json.dumps(json_data)
+
 
 #the index file has to be in a dir named templates in webapp
 #creates the table sub page
@@ -16,6 +43,10 @@ def index():
 def tables():
     return render_template('index.html')
 
-#runs the app
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
+
+app.run()
