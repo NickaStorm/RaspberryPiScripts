9ddead1fb8501bd7fd11027ea004c5940678cb14
NickaStorm <nicka@ai-engineered.com>
2021-08-20 11:07:34 -0600

    adjusting graph visuals

--- Website/app.py
+++ Website/app.py
@@ -40,7 +40,11 @@ def index():
     x1 = [1, 2, 3, 4]
     y1 = [2, 6, 1, 3]
     axis.plot(x1, y1)
-    plt.rcParams['figure.facecolor'] = 'black'
+    plt.ylabel("Dividend Yield", fontweight='bold')
+    plt.xlabel("Date", fontweight='bold')
+    ax = plt.axes()
+    ax.set_facecolor("yellow")
+    plt.title("Dividend Stock Yields")
 
     graph = mpld3.fig_to_html(fig)
     return """
