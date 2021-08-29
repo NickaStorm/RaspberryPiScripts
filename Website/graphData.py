401b25b8c648a96bcefb6622dc6c736aca646745
NickaStorm <nicka@ai-engineered.com>
2021-08-29 13:54:12 -0600

    final draft of graphdata insertion script

--- Website/graphData.py
+++ Website/graphData.py
@@ -6,7 +6,6 @@ import mysql.connector
 temptime = time.strftime('%Y-%m-%d')
 curDate = datetime.date.today()
 Date1 = curDate - datetime.timedelta(days=1)
-# Date2 = curDate - datetime.timedelta(days=2)
 sqlData = []
 
 conn = mysql.connector.connect(
@@ -19,20 +18,7 @@ conn = mysql.connector.connect(
     )
 cur = conn.cursor(prepared=True)
 graphData_query = """ INSERT INTO graphdata
-                       (name, currentprice, forwardPE, sector, curDate) VALUES (%s,%s,%s,%s,%s)"""
-
-# cur.execute("select stickersymbol from stockinfo")
-# rv = cur.fetchall()
-# for result in rv:
-#     sqlData.append(result)
-#     print(result)
-
-# tickerSymbol = 'DOV'
-
-# tickerData = yf.Ticker(tickerSymbol)
-# stockInfo = [tickerData.info["shortName"], tickerData.info["currentPrice"], tickerData.info["forwardPE"], tickerData.info["sector"], curDate]
-
-# indexNum = 0
+                       (ticker, name, currentprice, forwardPE, sector, curDate) VALUES (%s,%s,%s,%s,%s,%s)"""
 
 def getTickerData(ticker, stockNum):
 
@@ -50,7 +36,7 @@ def getTickerData(ticker, stockNum):
 
     print(ticker)
     tickerData = yf.Ticker(ticker)
-    listOfTicker = [tickerData.info["shortName"], tickerData.info["currentPrice"], tickerData.info["forwardPE"], tickerData.info["sector"], temptime]
+    listOfTicker = [ticker, tickerData.info["shortName"], tickerData.info["currentPrice"], tickerData.info["forwardPE"], tickerData.info["sector"], temptime]
     print(str(stockNum) + " " + ticker + " = " + tickerData.info["shortName"])
     return listOfTicker
 
@@ -62,12 +48,8 @@ for result in rv:
 indexNum = 1
 
 for item in sqlData:
-    # stockTicker = item[0].replace(".", "")
     cur.execute(graphData_query, getTickerData(item[0], indexNum))
-    # print(item[0])
     indexNum += 1
-
-print(sqlData)
 
 conn.commit()
 conn.close()
