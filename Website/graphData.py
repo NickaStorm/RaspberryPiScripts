529b1ff2a60dcc3b8785529376a2a58cf7be9cae
NickaStorm <nicka@ai-engineered.com>
2021-08-28 18:48:06 -0600

    first attempt at insertion graph data script

--- Website/graphData.py
+++ Website/graphData.py
@@ -1,3 +1,38 @@
 import yfinance as yf
+import datetime
+import mysql.connector
 
-tickersymbol = 'DOV'
+curDate = datetime.date.today()
+Date1 = curDate - datetime.timedelta(days=1)
+# Date2 = curDate - datetime.timedelta(days=2)
+tickerSymbol = 'DOV'
+sqlData = []
+
+conn = mysql.connector.connect(
+        host='localhost',
+        user='user',
+        password='blueberry',
+        # port=3306,
+        database='dividendchampions',
+        auth_plugin='mysql_native_password'
+    )
+cur = conn.cursor(prepared=True)
+graphData_query = """ INSERT INTO graphata
+                       (name, currentprice, forwardPE, sector, curDate) VALUES (%s,%s,%s,%s,%s)"""
+
+cur.execute("select stickersymbol from stockinfo")
+    rv = cur.fetchall()
+    for result in rv:
+        sqlData.append(result)
+
+tickerData = yf.Ticker(tickerSymbol)
+stockInfo = [tickerData.info["shortName"], tickerData.info["currentPrice"], tickerData.info["forwardPE"], tickerData.info["sector"], curDate]
+
+for item in sqlData:
+    cur.execute(graphData_query, stockInfo)
+    print(item)
+
+conn.commit()
+conn.close()
+
+
